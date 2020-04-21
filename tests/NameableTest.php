5aca136424feea84e78e467cbe1f14f2467f82eb
Dwight Watson <dwightwatson@users.noreply.github.com>
2020-04-21 15:48:30 +1000

    Add cast tests

---
+++ tests/NameableTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Watson\Nameable\Tests;
+
+use Watson\Nameable\Name;
+use Watson\Nameable\Nameable;
+
+class NameableTest extends TestCase
+{
+    /** @test */
+    public function it_gets_name_instance()
+    {
+        $result = (new Nameable)->get(null, null, 'Dwight Conrad Watson', null);
+
+        $this->assertInstanceOf(Name::class, $result);
+
+        $this->assertEquals('Dwight Conrad Watson', (string) $result);
+    }
+
+    /** @test */
+    public function it_casts_name_instance_back_to_string()
+    {
+        $name = Name::from('Dwight Conrad Watson');
+
+        $result = (new Nameable)->set(null, null, $name, null);
+
+        $this->assertIsString($result);
+
+        $this->assertEquals('Dwight Conrad Watson', $result);
+    }
+}
